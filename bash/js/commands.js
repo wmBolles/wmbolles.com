218cf2cd8022e9f318efff7077acdb0d4c292f4a
TheF <wassimbolles18@gmail.com>
2024-09-14 05:04:04 +0100

    latest codebase version

---
+++ bash/js/commands.js
@@ -0,0 +1,17 @@
+var dev="#",twitter="#",linkedin="https://www.linkedin.com/in/wassim-bolles-60a47523a/",instagram="https://www.instagram.com/wassim_bolles/",github="https://github.com/wmbolles",sudo="https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1",email="wabolles@student.1337.ma";aboutme=["<br>","<span> Hey, I'm wassim bolles! \uD83D\uDC4B</span>","<br>","<li>\uD83D\uDD2D I’m currently a student at 42 network | 1337 school.</li>","<br>",],social=["<br>",'linkedin       <a href="'+linkedin+'" target="blank">linkedin.com/in/wassim-bolles-60a47523a/</a>','instagram      <a href="'+instagram+'" target="blank">instagram/wassim_bolles</a>','github         <a href="'+github+'" target="blank">github/wmbolles</a>',"<br>",],projects=["<br>","Coming soon...","<br>",],help=[`<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,],banner=[`<div class="pt-2"><span class="text-[#7d82d7db] ">-—> Type <span class="command text-[#75e1e7]">help</span> !</span></div>`,];
